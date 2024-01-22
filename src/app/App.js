cb8daced297ba17ddeaa795db08df5f44fc385de
Dan <rabota1515@gmail.com>
2024-01-22 18:03:33 +0300

    Start project

---
+++ src/app/App.js
@@ -0,0 +1,17 @@
+import Header from '../pages/header/Header';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <main>
+          <Header />
+          <div>App</div>
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/header/Header.js
@@ -0,0 +1,16 @@
+import Icons from '../../assets/icons/svg/Icons';
+
+import styles from './Header.module.scss';
+
+const Header = () => {
+  return (
+    <header>
+      <div>Header</div>
+      <Icons id="icon-header" />
+      {/* <Icons name="icon-header" /> */}
+      <div></div>
+    </header>
+  );
+};
+
+export default Header;
